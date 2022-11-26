422c76b6bbcb07236f6c09ba3d4d4d034349871b
JacksonMcKay28 <111894468+JacksonMcKay28@users.noreply.github.com>
2022-11-26 03:13:40 +0000

    first test

--- .ccls-cache/@home@runner@HW5P4ModuloJTM/main.cpp
+++ .ccls-cache/@home@runner@HW5P4ModuloJTM/main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-  std::cout << "Hello World!\n";
+  int values[10] = {};
+
+  for (int i = 0; i < 10; i++) 
+  {
+    cin >> values[i];
+  }
+
+  int sum = 0;
+
+  for (int j = 0; j < 10; j++) 
+  {
+    if ((values[j] % 42) > 0)
+      sum = sum + 1;
+    else;
+
+  }
+  cout << sum
 }
